db21e51292fe7edae9fa1768e6e3e56675988243
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-05 10:36:11 +0530

    created static files for home page

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express= require('express')
 const app =express()
 const port=8000
 
+
+app.use('/',require('./Routes'))
+
+// setting our view engine ejs
+app.set('view engine','ejs')
+app.set('path','./views')
+app.use(express.static('./assets'));
+
+
 app.listen(port,function(err)
 {
     if(err)
